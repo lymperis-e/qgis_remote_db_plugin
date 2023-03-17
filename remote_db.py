d186e9e0238869c9279a099b720affacda2156bf
elymperis <elymperis@getmap.gr>
2023-03-17 22:30:57 +0200

    New logo

--- remote_db.py
+++ remote_db.py
@@ -183,7 +183,7 @@ class RemoteDB:
     def initGui(self):
         """Create the menu entries and toolbar icons inside the QGIS GUI."""
 
-        icon_path = ':/plugins/remote_db/assets/icon.png'
+        icon_path = ':/plugins/remote_db/icon.png'
         self.add_action(
             icon_path,
             text=self.tr(u'Open SSH tunnels'),
